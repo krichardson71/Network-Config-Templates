85201faf5e2af4d04b0503f187aa82fc8446bfe8
krichardson71 <40860386+krichardson71@users.noreply.github.com>
2019-12-31 15:05:26 -0500

    ciscoVlan

--- ciscoVlan.py
+++ ciscoVlan.py
@@ -11,26 +11,29 @@ change_type = input('change or rollback: ').lower()
 args = {'username': 'krrichar', 'password': password,
         'device_type': 'cisco_nxos'}
 
-hosts = ['us6645ny-core-vdc1', 'us6645ny-core-vdc2',
-         'us6647ny-core-vdc1', 'us6647ny-core-vdc2',
-         'us9485ne-core-vdc1', 'us9485ne-core-vdc2']
+hosts = ['us6645ny-ppcore-vdc1',
+         'us6647ny-ppcore-vdc1']
 
-wdc = {'vlan_id': '900', 
-       'vlan_name': 'VL900_WDC_PROD_128.1.6.5/24', 
-       'interfaces': 'interface Ethernet5/8'}
-
-hdc = {'vlan_id': '900', 
-       'vlan_name': 'VL900_HDC_PROD_128.1.6.5/24',
-       'interfaces': 'interface Ethernet5/25'}
-
-odc = {'vlan_id': '900', 
-       'vlan_name': 'VL900_ODC_PROD_128.1.6.5/24',
-       'interfaces': 'interface Ethernet5/25'}
+wdc = {'num': '900',
+       'name': 'VL900_WDC_N2A_128.1.6.5/24',
+       'interfaces': 'Ethernet1/3, Ethernet1/6, Ethernet1/10-13,\
+                      Ethernet5/3, Ethernet5/6, Ethernet5/10-13'}
+hdc = {'num': '900',
+       'name': 'VL900_HDC_N2A_128.1.6.5/24',
+       'interfaces': 'Ethernet1/3, Ethernet1/6-7, Ethernet1/10, Ethernet1/12, Ethernet1/15,\
+                      Ethernet5/3, Ethernet5/6-7, Ethernet5/10, Ethernet5/12, Ethernet5/15'}
 
 
 def main():
 
     def change(num, name, interface):
+        """
+        @param num: vlan id
+        @param name: vlan name
+        @param interface: trunk interface(s) to add vlan
+        @return: None
+
+        """
         net_connect = ConnectHandler(host, session_log=host, **args)
         print(net_connect.find_prompt())
         net_connect.send_command('show run')
@@ -44,9 +47,16 @@ def main():
                                      'switchport trunk allowed vlan add {}'.format(num)])
         postcheck = net_connect.send_command('show vlan id {}'.format(num))
         print(postcheck)
+        net_connect.send_command('copy running-config startup-config')
         net_connect.disconnect()
 
     def rollback(num, interface):
+        """
+        @param num: vlan id
+        @param interface: trunk interface(s) to add vlan
+        @return: None
+
+        """
         net_connect = ConnectHandler(host, session_log=host, **args)
         print(net_connect.find_prompt())
         net_connect.send_command('show run')
@@ -59,30 +69,28 @@ def main():
                                      'switchport trunk allowed vlan remove {}'.format(num)])
         postcheck = net_connect.send_command('show vlan id {}'.format(num))
         print(postcheck)
+        net_connect.send_command('copy running-config startup-config')
         net_connect.disconnect()
 
     for host in hosts:
-        try:  # webster
+
+        #  webster
+        try:
             if ('6645ny' in host) and (change_type == 'change'):
                 change(num=wdc['num'], name=wdc['name'], interface=wdc['interfaces'])
             elif ('6645ny' in host) and (change_type == 'rollback'):
                 rollback(num=wdc['num'], interface=wdc['interfaces'])
         except (EOFError, SSHException) as error:
             print(error, '\n\t## webster changes unsuccessful ##')
-        try:  # henrietta
+
+        #  henrietta
+        try:
             if ('6647ny' in host) and (change_type == 'change'):
-                change(num=hdc['num'], name=hdc['name'], interface=hdc['interfaces'])
+                change(num=wdc['num'], name=wdc['name'], interface=wdc['interfaces'])
             elif ('6647ny' in host) and (change_type == 'rollback'):
                 rollback(num=hdc['num'], interface=hdc['interfaces'])
         except (EOFError, SSHException) as error:
             print(error, '\n\t## henrietta changes unsuccessful ##')
-        try:  # omaha
-            if ('9485ne' in host) and (change_type == 'change'):
-                change(num=odc['num'], name=odc['name'], interface=odc['interfaces'])
-            elif ('9485ne' in host) and (change_type == 'rollback'):
-                rollback(num=odc['num'], interface=odc['interfaces'])
-        except (EOFError, SSHException) as error:
-            print(error, '\n\t## omaha changes unsuccessful ##')
 
         print('\n\t## changes complete for {} ##'.format(host))
 
